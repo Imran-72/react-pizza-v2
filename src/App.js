51007e027c756295d111ed200d4649d1272c89ab
iabdurakhmanov <iabdurakhmanov@iterion.ru>
2022-08-07 15:13:41 +0300

    render PizzaBlock component

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './scss/app.scss';
+
+import { Header } from './components/Header';
+import { Categories } from './components/Categories';
+import { SortPizzas } from './components/SortPizzas';
+import { PizzaBlock } from './components/PizzaBlock';
+import pizzas from './assets/pizzas.json';
+
+function App() {
+  return (
+    <>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <SortPizzas />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              {pizzas.map(({ id, ...rest }) => (
+                <PizzaBlock key={id} {...rest} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
